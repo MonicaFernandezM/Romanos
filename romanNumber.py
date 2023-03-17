ecb663bc87d6eaa9dc1428de2a050526860b52e0
monicamfm <monicamfmiras@gmail.com>
2023-03-17 22:28:21 +0100

    clase romanNumber y numeros magicos

--- romanNumber.py
+++ romanNumber.py
@@ -10,3 +10,16 @@ class RomanNumber():
             self.arabigo = entrada 
         else: #me meten un tupla, una lista
             raise RomanError("La entrada tiene que ser entero o cadena")
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def __str__(self): #siempre devuelve una cadena
+        return f'{self.romano}'
+    
+    def __lt__(self, entrada):
+        if not isinstance(entrada, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        
+        return self.arabigo < entrada.arabigo
+    
